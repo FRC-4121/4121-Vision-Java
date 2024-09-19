2c1095f1d969f8a1c8fa9f7a53243b0cf703890f
Matthew Cornell <mcornell400708@gmail.com>
2024-09-19 13:29:39 -0400

    Make some small changes to camera config

--- src/main/java/frc/vision/camera/CameraConfig.java
+++ src/main/java/frc/vision/camera/CameraConfig.java
@@ -1,16 +1,13 @@
 import java.util.ArrayList;
 
-// POJO for camera config
+// Bean for camera config
 class CameraConfig {
     public int width;
     public int height;
     public int fps;
     public float fov;
     public ArrayList<String> vlibs;
-
     public int cropBottom;
-    public Integer port;
-    public String path;
 
     public void updateFrom(CameraConfig other) {
         if (width == 0) width = other.width;
