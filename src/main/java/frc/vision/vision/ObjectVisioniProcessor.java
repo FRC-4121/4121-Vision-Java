bc5bcf2b84fd722ea9b952d55e8bbf436900847e
Matthew Cornell <mcornell400708@gmail.com>
2024-09-19 09:28:17 -0400

    Implement vision base classes

---
+++ src/main/java/frc/vision/vision/ObjectVisioniProcessor.java
@@ -0,0 +1,50 @@
+import edu.wpi.first.networktables.*;
+import java.util.List;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+
+/// Most vision processors find some kind of bounding rectangle around their objects.
+abstract class ObjectVisionProcessor extends VisionProcessor {
+    protected List<VisionObject> objects;
+    public Scalar rectColor;
+    ObjectVisionProcessor(String name) {
+        super(name);
+        rectColor = new Scalar(1.0, 0.0, 0.0);
+    }
+    ObjectVisionProcessor(String name, Scalar color) {
+        super(name);
+        rectColor = color;
+    }
+    /// Process the input image into a list of objects
+    protected abstract List<VisionObject> processObjects(Mat img, CameraConfig cfg);
+    public void process(Mat img, CameraConfig cfg) {
+        objects = processObjects(img, cfg);
+    }
+    public void toNetworkTable(NetworkTable table) {
+        int i = 0;
+        int size = objects.size();
+        long[] x = new long[size];
+        long[] y = new long[size];
+        long[] w = new long[size];
+        long[] h = new long[size];
+        double[] o = new double[size];
+        for (VisionObject obj : objects) {
+            x[i] = obj.x;
+            y[i] = obj.y;
+            w[i] = obj.width;
+            h[i] = obj.height;
+            o[i] = obj.offset();
+        }
+        table.putValue("x", NetworkTableValue.makeIntegerArray(x));
+        table.putValue("y", NetworkTableValue.makeIntegerArray(y));
+        table.putValue("w", NetworkTableValue.makeIntegerArray(w));
+        table.putValue("h", NetworkTableValue.makeIntegerArray(h));
+        table.putValue("o", NetworkTableValue.makeDoubleArray(o));
+        table.putValue("len", NetworkTableValue.makeInteger(size));
+    }
+    public void drawOnImage(Mat img) {
+        for (VisionObject obj : objects) {
+            Imgproc.rectangle(img, obj.tl(), obj.br(), rectColor, 1);
+        }
+    }
+}
---
+++ src/main/java/frc/vision/vision/VisionObject.java
@@ -0,0 +1,25 @@
+import org.opencv.core.*;
+
+/// The base vision object is just a rectangle with a name.
+class VisionObject extends Rect {
+    VisionObject() {}
+    VisionObject(int x, int y, int w, int h) {
+        super(x, y, w, h);
+    }
+    VisionObject(Point p1, Point p2) {
+        super(p1, p2);
+    }
+    VisionObject(Point p, Size s) {
+        super(p, s);
+    }
+    VisionObject(Rect r) {
+        super(r.x, r.y, r.width, r.height);
+    }
+    public String getName() {
+        return "unknown";
+    }
+    public double offset() {
+        // TODO
+        return 0;
+    }
+}
