55d9104461c02e82ae823ffca00263379e1436be
Matthew Cornell <mcornell400708@gmail.com>
2025-01-09 16:59:53 -0500

    Make the camera able to order processor dependencies (tentatively)

--- src/main/java/frc/vision/pipeline/VisionLibsGroup.java
+++ src/main/java/frc/vision/pipeline/VisionLibsGroup.java
@@ -3,19 +3,24 @@ package frc.vision.pipeline;
 import edu.wpi.first.networktables.NetworkTable;
 import frc.vision.camera.*;
 import frc.vision.process.*;
-import java.lang.Void;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.opencv.core.Mat;
 
 // Vision library group to handle dispatch from a frame to running vision processors.
 // Should be mostly non-blocking.
 public class VisionLibsGroup implements BiConsumer<Mat, CameraBase> {
-    protected static class CamState {
+    protected class CamState {
         CompletableFuture<Void> handle;
+        ArrayList<ArrayList<Integer>> plan;
         Mat frame;
         boolean loggedLibs;
 
@@ -24,17 +29,93 @@ public class VisionLibsGroup implements BiConsumer<Mat, CameraBase> {
             frame = new Mat();
             loggedLibs = false;
         }
+
+        public void buildPlan(Collection<String> vlibs) {
+            if (plan == null) rebuildPlan(vlibs);
+        }
+
+        public void rebuildPlan(Collection<String> vlibs) {
+            class Proc {
+                int index;
+                Collection<String> deps;
+                Collection<Integer> resDeps;
+
+                Proc(int index) {
+                    this.index = index;
+                }
+                Proc(int index, Collection<String> deps) {
+                    this.index = index;
+                    this.deps = deps;
+                }
+            }
+            plan = new ArrayList(1);
+            HashMap<String, Proc> map = new HashMap<>();
+            HashSet<Integer> seen = new HashSet<>();
+            int idx = 0;
+            boolean again = false;
+
+            for (VisionProcessor proc : procs) {
+                ProcessorConfig cfg = proc.getConfig();
+                if (vlibs != null && !vlibs.contains(proc.getName())) {
+                    seen.add(idx);
+                    map.put(proc.getName(), new Proc(idx));
+                } else if (cfg == null || cfg.deps == null || cfg.deps.size() == 0) {
+                    if (plan.isEmpty()) plan.add(new ArrayList<>());
+                    plan.get(0).add(idx);
+                    seen.add(idx);
+                    map.put(proc.getName(), new Proc(idx));
+                } else {
+                    map.put(proc.getName(), new Proc(idx, cfg.deps.values()));
+                    again = true;
+                }
+                idx++;
+            }
+            for (var entry : map.entrySet()) {
+                if (entry.getValue().deps == null) continue;
+                entry.getValue().resDeps = entry.getValue().deps.stream()
+                    .map(d -> {
+                        Proc p = map.get(d);
+                        if (p == null) {
+                            System.err.println("Referred to missing processor \"" + d + "\"");
+                            return null;
+                        } else return p.index;
+                    })
+                    .filter(i -> i != null && !seen.contains(i))
+                    .collect(Collectors.toList());
+            }
+
+            if (plan.isEmpty() && again) {
+                System.err.println("Cycle in processor dependencies");
+            }
+
+            for (int i = 1; again; i++) {
+                again = false;
+                for (Proc proc : map.values()) {
+                    if (proc.resDeps != null && proc.resDeps.isEmpty()) {
+                        again = true;
+                        if (plan.size() == i) plan.add(new ArrayList<>());
+                        plan.get(i).add(proc.index);
+                        proc.resDeps = null;
+                        for (Proc p2 : map.values()) if (p2.resDeps != null) p2.resDeps.remove(proc.index);
+                    }
+                }
+            }
+
+            for (Proc proc : map.values()) if (proc.resDeps != null) {
+                System.err.println("Cycle in processor dependencies");
+            }
+        }
     }
     Executor exec;
     CameraBase lastHandle;
-    Collection<VisionProcessor> procs;
+    List<VisionProcessor> procs;
     NetworkTable table;
     BiConsumer<Mat, ? super CameraBase> postProcess;
     ConcurrentHashMap<CameraBase, CamState> states;
     boolean visionDebug;
     boolean cloneFrame;
 
-    public VisionLibsGroup(Collection<VisionProcessor> procs, NetworkTable table, boolean visionDebug, Executor exec) {
+    public VisionLibsGroup(List<VisionProcessor> procs, NetworkTable table, boolean visionDebug, Executor exec) {
         this.procs = procs;
         this.exec = exec;
         this.table = table;
@@ -93,14 +174,19 @@ public class VisionLibsGroup implements BiConsumer<Mat, CameraBase> {
                 cam.getLog().flush();
                 state.loggedLibs = true;
             }
-            // if (!cloneFrame) cam.getLock().lock();
+            state.buildPlan(cam.getConfig().vlibs);
             Mat frame = cloneFrame ? state.frame.clone() : state.frame;
-            CompletableFuture<Void> future = CompletableFuture.allOf(
-                getLibs(cam.getConfig().vlibs)
-                    .map(proc -> CompletableFuture.runAsync(() -> proc.process(frame, cam), exec))
-                    .toArray(size -> new CompletableFuture[size])
-            );
-            // if (!cloneFrame) future = future.whenComplete((_void, _ex) -> cam.getLock().unlock());
+            CompletableFuture<Void> future = CompletableFuture.completedFuture(null);
+            for (int i = 0; i < state.plan.size(); i++) {
+                int j = i; // make java happy about final variables
+                future = future.thenCompose(_void -> {
+                    return CompletableFuture.allOf(
+                        state.plan.get(j).stream()
+                            .map(idx -> CompletableFuture.runAsync(() -> procs.get(idx).process(frame, cam), exec))
+                            .toArray(size -> new CompletableFuture[size])
+                    );
+                });
+            }
             if (table != null || visionDebug) {
                 future = future.thenCompose(_void -> {
                     Stream<CompletableFuture<Void>> tables = Stream.empty();
