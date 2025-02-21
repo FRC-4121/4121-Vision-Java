8578343988645216ccf7187e053d141c27c215da
Matthew Cornell <mcornell400708@gmail.com>
2025-02-21 18:28:47 -0500

    Make `RingBuffer` actually be a queue

--- src/main/java/frc/vision/pipeline/RingBuffer.java
+++ src/main/java/frc/vision/pipeline/RingBuffer.java
@@ -1,5 +1,7 @@
 package frc.vision.pipeline;
 
+import java.util.AbstractQueue;
+import java.util.Iterator;
 import org.opencv.core.Mat;
 
 /**
@@ -7,7 +9,7 @@ import org.opencv.core.Mat;
  *
  * If the capacity is exceeded, the oldest elements are removed. Note that this doesn't actually implement Queue because I'm too lazy to implement all of the Collection methods
  */
-public class RingBuffer {
+public class RingBuffer extends AbstractQueue<Mat> {
     protected Mat[] elems;
     protected int start;
     protected int end;
@@ -24,13 +26,15 @@ public class RingBuffer {
     private int modulus() {
         return elems.length;
     }
+    @Override
     public synchronized int size() {
         int len = end - start;
         if (len < 0) len += modulus();
         return len;
     }
 
-    public synchronized boolean add(Mat elem) {
+    @Override
+    public synchronized boolean offer(Mat elem) {
         if (elems[end] == null) elems[end] = new Mat();
         elem.copyTo(elems[end]);
         if (++end == modulus()) {
@@ -39,32 +43,37 @@ public class RingBuffer {
         if (start == end && ++start == modulus()) start = 0;
         return true;
     }
-    public boolean offer(Mat elem) {
-        return add(elem);
-    }
-    public synchronized Mat remove() {
-        if (start == end) throw new IllegalStateException("Attempted to remove from an empty queue");
-        Mat out = elems[start++];
-        if (start == modulus()) start = 0;
-        return out;
-    }
+    @Override
     public synchronized Mat poll() {
         if (start == end) return null;
         Mat out = elems[start++];
         if (start == modulus()) start = 0;
         return out;
     }
-    public synchronized Mat element() {
-        if (start == end) throw new IllegalStateException("Attempted to get first element of an empty queue");
-        return elems[start];
-    }
+    @Override
     public synchronized Mat peek() {
         return start == end ? null : elems[start];
     }
 
+    @Override
     public void clear() {
         start = 0;
         end = 0;
     }
-
+    @Override
+    public Iter iterator() {
+        return new Iter();
+    }
+    
+    public class Iter implements Iterator<Mat> {
+        @Override
+        public boolean hasNext() {
+            return size() == 0;
+        }
+        @Override
+        public Mat next() {
+            return poll();
+        }
+    }
 }
+
